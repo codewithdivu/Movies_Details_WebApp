2586ca63fd7a9509498cdfdf91319dd7eee7ef09
MAVADIYA DIVYESHKUMAR <mavadiyadivyesh56@gmail.com>
2022-03-13 22:21:32 +0530

    first commit of this project

---
+++ src/Components/Movies.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { getMovies } from "../fakeMovieService";
+import Like from "./common/like";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    if (this.state.movies.length === 0) {
+      return <h1>There are no Movies left in table</h1>;
+    }
+
+    return (
+      <div>
+        <h2 className="m-3">there are {this.state.movies.length} are left</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Titles</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like />
+                </td>
+                <td>
+                  {
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  }
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
