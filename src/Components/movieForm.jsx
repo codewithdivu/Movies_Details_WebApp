2168a6aecb0e5651a7bbc1d0152618e386b4f6d8
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-03-27 10:27:29 +0530

    upto this new Movie button is set

--- src/Components/movieForm.jsx
+++ src/Components/movieForm.jsx
@@ -1,19 +1,93 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { getMovie, saveMovie } from "../fakeMovieService";
+import { getGenres } from "../fakeGenreService";
 
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    genres: [],
+    errors: {},
+  };
 
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.string().required().min(0).max(100).label("Title"),
+    dailyRentalRate: Joi.string().min(0).max(10).required().label("Title"),
+  };
 
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1>MovieFrom {match.params.id}</h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
+      title: movie.title,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate,
+    };
+  }
+
+  doSubmit = () => {
+    saveMovie(this.state.data);
+
+    this.props.history.push("/movies");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate")}
+          {this.renderButton("Save")}
+        </form>
+        {/* <button
+          className="btn btn-primary"
+          onClick={() => this.state.history.push("/movies")}
+        >
+          Save
+        </button> */}
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
+
+// const MovieForm = ({ match, history }) => {
+//   return (
+//     <div>
+//       <h1>MovieFrom {match.params.id}</h1>
+//       <button
+//         className="btn btn-primary"
+//         onClick={() => history.push("/movies")}
+//       >
+//         Save
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default MovieForm;
